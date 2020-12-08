70167917f52ce267ad47556f94c65d4ff83435d1
Aturt2 <antoniomartinez222@hotmail.com>
2020-12-08 20:11:28 +0100

    Paths to tables are now relative to repo root

--- utils/find_all_colnames.py
+++ utils/find_all_colnames.py
@@ -1,16 +1,25 @@
 import glob
 import pandas as pd
+import os
 from pathlib import Path
 
-if __name__ == "__main__":
-    files = glob.glob(r"../*/**/[a-zA-Z]*.xlsx", recursive=True)
+def run():
+    files = glob.glob(r"{}/../*/**/[a-zA-Z]*.xlsx".format(str(Path(__file__).parent)), recursive=True)
 
     paths = []
     cols = []
 
     for file in files:
         df = pd.read_excel(file)
-        paths.extend([file] * len(df.columns))
         cols.extend(df.columns)
 
+        # Make path relative to repo root
+        parts = Path(file).absolute().resolve().parts
+        path = os.path.join(*parts[parts.index("typology_of_crosslingual") + 1:])
+        paths.extend([path] * len(df.columns))
+
     pd.DataFrame({"path": paths, "colname": cols}).to_csv(Path(__file__).parent / "all_colnames.tsv", index=False, sep="\t")
+    print("Updated utils/all_colnames.tsv")
+
+if __name__ == "__main__":
+    run()
